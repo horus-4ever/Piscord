c2226259a01e1f6afb577ba5e4a5f3466bd62c12
horus-4ever <th.pichard54@gmail.com>
2020-10-13 23:16:55 +0200

    Improved the Errors part

--- piscord/Errors.py
+++ piscord/Errors.py
@@ -1,27 +1,18 @@
 class Error(Exception):
-
-	def __init__(self):
-		self.error = "Unknown Error"
-
+	error = "Unknown Error"
 	def __str__(self):
 		return self.error
 
 class PermissionsError(Error):
-
-	def __init__(self):
-		self.error = "You do not have permission to do this action"
+	error = "You do not have permission to do this action"
 
 class BadRequestError(Error):
-
-	def __init__(self):
-		self.error = "Your query was incomplete or bad"
+	error = "Your query was incomplete or bad"
 
 class TokenError(Error):
-
-	def __init__(self):
-		self.error = "The token is not valid"
+	error = "The token is not valid"
 
 class ConnexionError(Error):
-
+	error = "Connexion Error : {}"
 	def __init__(self, error):
-		self.error = f"Connexion Error : {error}"
+		self.error = self.error.format(error)
