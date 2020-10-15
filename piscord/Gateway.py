e0253f38fa0fe4202089159292a53387d17682cc
horus-4ever <th.pichard54@gmail.com>
2020-10-15 19:12:27 +0200

    Improved lisbility of the gateway

--- piscord/Gateway.py
+++ piscord/Gateway.py
@@ -16,51 +16,62 @@ class Gateway:
 		self.session_id = None
 		self.presence = presence
 		self.error = None
+		self._running = False
+
+	async def _on_connection_closed(self):
+		"""
+		When the bot loses the connection, we try to reconnect 5 times.
+		If it fails, then we must exit.
+		"""
+		await self.websocket.close()
+		if self.session_id and self.last_sequence:
+			await self.websocket.close()
+			for reconnect_attemps in range(5):
+				websocket, msg = await self._reconnect()
+				if msg:
+					data = json.loads(msg)
+					self.websocket = websocket
+					self.interval = data["d"]["heartbeat_interval"]
+					break
+				else:
+					await asyncio.sleep(1)
+			else:
+				self.error = ConnexionError("You've lost the connection to the server")
+				self.heartbeat.cancel()
+		else:
+			self.heartbeat.cancel()
+			self._running = False
 
 	async def connect(self, action = None, shards = [0,1]):
+		self._running = True
 		self.loop = asyncio.get_event_loop()
-		ws = await websockets.connect(self.url, ping_interval = None, max_size=1_000_000_000)
-		self.ws = ws
-		while True:
+		self.websocket = await websockets.connect(self.url, ping_interval = None, max_size=1_000_000_000)
+
+		while self._running:
 			if self.error:
 				raise self.error
 			try:
-				msg = await self.ws.recv()
-			except Exception as e:
-				if e.code in [1000, 1001, 1006]:
-					await self.ws.close()
-					if self.session_id and self.last_sequence:
-						await self.ws.close()
-						for i in range(5):
-							ws,msg = await self._reconnect()
-							if msg:
-								data = json.loads(msg)
-								self.ws = ws
-								self.interval = data["d"]["heartbeat_interval"]
-								break
-							else:
-								await asyncio.sleep(1)
-						else:
-							self.error = ConnexionError("You've lost the connection to the server")
-							self.heartbeat.cancel()
-						continue
-					else:
-						self.heartbeat.cancel()
-						return
-				elif e.code == 4004:
+				msg = await self.websocket.recv()
+			except Exception as error_code:
+				if error_code.code in (1000, 1001, 1006): # 1006 => connection closed
+					await self._on_connection_closed()
+					continue
+				elif error_code.code == 4004: # 4004 => invalid token
 					self.error = TokenError()
 				else:
 					self.error = ConnexionError(f"Unknow Error - {e.code}")
 				self.heartbeat.cancel()
 				continue
 			
+			# if there were no errors, we decode the data
 			data = json.loads(msg)
-			x = data.get("s")
-			if x:
-				self.last_sequence = x
+			# print(data)
+			sequence = data.get("s")
+			if sequence:
+				self.last_sequence = sequence
 			if data["op"] == self.auth_op:
 				self.interval = data["d"]["heartbeat_interval"]
-				self.heartbeat = asyncio.create_task(self.__heartbeat(self.ws))
+				self.heartbeat = asyncio.create_task(self.__heartbeat(self.websocket))
 				payload = {
 					"op": 2,
 					"d": {
@@ -92,28 +103,28 @@ class Gateway:
 				"session_id": self.session_id,
 				"seq": self.last_sequence
 		}}
-		ws = await websockets.connect(self.url, ping_interval = None)
+		websocket = await websockets.connect(self.url, ping_interval = None)
 		msg = None
 		try:
-			await ws.recv()
+			await websocket.recv()
 			await self.send(payload)
-			msg = await ws.recv()
+			msg = await websocket.recv()
 		except Exception as e:
-			await ws.close()
-		return ws, msg
+			await websocket.close()
+		return websocket, msg
 
 	async def __heartbeat(self, ws):
 		while True:
 			await asyncio.sleep(self.interval / 1000)
-			if not self.ws.closed:
+			if not self.websocket.closed:
 				await self.send({"op": self.heartbeat_code,"d": self.last_sequence})
 
 	async def send(self, payload):
-		await self.ws.send(json.dumps(payload))
+		await self.websocket.send(json.dumps(payload))
 
 	async def _stop(self):
 		self.session_id = None
-		await self.ws.close()
+		await self.websocket.close()
 		self.heartbeat.cancel()
 
 	def stop(self):
